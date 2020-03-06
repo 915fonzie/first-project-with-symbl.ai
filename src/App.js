22291eaafdf512dbfc55d0957797eb36cb165434
915Fonzie <alfonso.pru@outlook.com>
2020-03-06 01:50:23 -0800

    created Home component and set it up to view with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Route path='/' render={routerProps => <Home {...routerProps}/>} />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import {appIdAndSecret} from '../services/appIdAndSecret' //if forking project, will have to make your own file to use your appId and appSecret keys
+
+const Home = () => {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Home;
